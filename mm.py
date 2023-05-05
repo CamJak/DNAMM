7e42ab9b68ad1d6df4b88b1fe38029d74177f8d2
Blake <blake.perrin13@gmail.com>
2023-05-05 14:18:33 -0500

    Working randomly generated MM

--- mm.py
+++ mm.py
@@ -1,14 +1,117 @@
 import random
-
 class MooreMachine:
     def __init__(self) -> None:
-        self.__states = []
+        self.states = []
 
     class state:
-        def __init__(self, name, transitions) -> None:
-            self.__name = name
-            self.__transitions = transitions
+        def __init__(self, name, output, transitions = {}) -> None:
+            self.name = name
+            self.output = output
+            self.transitions = transitions
 
     def generate(self):
-        randNum = random.randint(0, 3)
+        output = ["A", "T", "C", "G"]
+        inputs = ["G", "A", "C", "T"]
+
+        regenerate = True
+        while regenerate:
+            input_G = [0, 1, 2, 3]
+            input_A = [0, 1, 2, 3]
+            input_C = [0, 1, 2, 3]
+            input_T = [0, 1, 2, 3]
+            self.states = []
+            for i in range(4):
+                self.states.append(self.state(i, output[i], {}))
+                usedNums = [0, 1, 2, 3]
+                for j in range(4):
+                    randNum = random.randint(0, 3)
+                    if j == 0:
+                        regenerate = False
+                        while randNum not in input_G or randNum not in usedNums:
+                            for num in input_G:
+                                if num not in usedNums:
+                                    regenerate = True
+                                else:
+                                    regenerate = False
+                                    break
+                            if regenerate:
+                                break
+                            randNum = random.randint(0, 3)
+                            regenerate = False
+                        if randNum in usedNums and randNum in input_G:
+                            input_G.remove(randNum)
+                            usedNums.remove(randNum)
+                    elif j == 1:
+                        regenerate = False
+                        while randNum not in input_A or randNum not in usedNums:
+                            for num in input_A:
+                                if num not in usedNums:
+                                    regenerate = True
+                                else:
+                                    regenerate = False
+                                    break
+                            if regenerate:
+                                break
+                            randNum = random.randint(0, 3)
+                            regenerate = False
+                        
+                        if randNum in usedNums and randNum in input_A:
+                            input_A.remove(randNum)
+                            usedNums.remove(randNum)
+                    elif j == 2:
+                        regenerate = False
+                        while randNum not in input_C or randNum not in usedNums:
+                            for num in input_C:
+                                if num not in usedNums:
+                                    regenerate = True
+                                else:
+                                    regenerate = False
+                                    break
+                            if regenerate:
+                                break
+                            randNum = random.randint(0, 3)
+                            regenerate = False
+                        if randNum in usedNums and randNum in input_C:
+                            input_C.remove(randNum)
+                            usedNums.remove(randNum)
+                    elif j == 3:
+                        regenerate = False
+                        while randNum not in input_T or randNum not in usedNums:
+                            for num in input_T:
+                                if num not in usedNums:
+                                    regenerate = True
+                                else:
+                                    regenerate = False
+                                    break
+                            if regenerate:
+                                break
+                            randNum = random.randint(0, 3)
+                            regenerate = False
+                            # if len(usedNums) == 1:
+                            #     if usedNums[0] not in input_T:
+                            #         regenerate = True
+                            #         break
+                            #     else:
+                            #         randNum = usedNums[0]
+                            #         regenerate = False
+                            # else:
+                            #     randNum = random.randint(0, 3)
+                            #     regenerate = False
+                        if randNum in usedNums and randNum in input_T:
+                            input_T.remove(randNum)
+                            usedNums.remove(randNum)
+                        if regenerate:
+                            break
+                    if regenerate:
+                        break
+                    if not regenerate:
+                        self.states[i].transitions.update({inputs[j]: randNum})
+                
         
+def main():
+    mm = MooreMachine()
+    mm.generate()
+    for state in mm.states:
+        print(f"State: {state.name}, Output: {state.output}, Transitions: {state.transitions}")
+
+main()
